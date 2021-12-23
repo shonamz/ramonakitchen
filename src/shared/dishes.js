e8a3cc3a7ac8a856982cf4a7a225d9bf15a2a6e4
shonamz <shokoufeh.namazian@gmail.com>
2021-12-23 10:48:12 -0800

    1

---
+++ src/shared/dishes.js
@@ -0,0 +1,43 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Mac and cheese',
+        image: '/assets/images/mac.jpg',
+        category: 'mains',
+        label:'Hot',
+        price:'4.99',
+        featured: true,
+        description:'This baked mac and cheese is a family favorite recipe, loved by both children and adults. It uses a combination of cheeses, layered in the dish as well as melted into a rich and creamy cheese sauce! '                    
+        },
+        {
+        id: 1,
+        name:'Cheese Toast',
+        image: '/assets/images/cheese.jpg',
+        category: 'appetizer',
+        label:'',
+        price:'1.99',
+        featured: false,
+        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+        },
+        {
+        id: 2,
+        name:'Ravioli',
+        image: '/assets/images/ravioli.jpg',
+        category: 'appetizer',
+        label:'New',
+        price:'1.99',
+        featured: false,
+        description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
+        },
+        {
+        id: 3,
+        name:'Cheese Cake',
+        image: '/assets/images/cheesecake.jpg',
+        category: 'dessert',
+        label:'',
+        price:'2.99',
+        featured: false,
+        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+        }
+    ];
---
+++ src/shared/leaders.js
@@ -0,0 +1,38 @@
+export const LEADERS = [
+    {
+      id: 0,
+      name: 'Peter Pan',
+      image: '/assets/images/chef4.jpg',
+      designation: 'Chief Epicurious Officer',
+      abbr: 'CEO',
+      featured: false,
+      description: "Our CEO, Peter, credits his hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. His mother's wizardy in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket."
+    },
+    {
+      id: 1,
+      name: 'Dhanasekaran Witherspoon',
+      image: '/assets/images/chef3.jpg',
+      designation: 'Chief Food Officer',
+      abbr: 'CFO',
+        featured: false,
+      description: 'Our CFO, Danny, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words.'
+    },
+    {
+      id: 2,
+      name: 'Agumbe Tang',
+      image: '/assets/images/chef2.jpg',
+      designation: 'Chief Taste Officer',
+      abbr: 'CTO',
+        featured: false,
+      description: 'Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, You click only if you survive my lick.'
+    },
+    {
+      id: 3,
+      name: 'Alberto Somayya',
+      image: '/assets/images/chef.jpg',
+      designation: 'Executive Chef',
+      abbr: 'EC',
+      featured: true,
+      description: 'Award winning three-star Michelin chef with wide International experience having worked closely with who in the culinary world, he specializes in creating mouthwatering sItalian fusion experiences  '
+    }
+  ];
